984b83857f842d7aa42f1aa0bd0e9196e2fbe4e0
Alex Miller <asmiller1989@gmail.com>
2015-11-18 00:30:25 -0600

    Update test.py

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ import requests
 import json
 import urllib2
 import timeit
+import os
 
 # set current process
 n = 1000
 start = timeit.default_timer()
-def getAzul():
+def GET():
 	try:
 	   	sql_data = json.dumps({"sql":1, "region":1})
 	   	mongo_data = json.dumps({"sql":0, "region":1})
@@ -34,41 +35,27 @@ def getAzul():
 		
 		
 		# Prototype system
-		
-		#Localhost
-		#url = "http://localhost:2050/get_deals"
-		# Dev server
-		#url = "azul@dev-02.azul.me:2050/get_deals"
 
 		# New dev server (only running prototype)
-		url = 'http://ubuntu@ec2-52-24-152-26.us-west-2.compute.amazonaws.com:2050/test'
+		url = os.environ['DEV_HOST']
 		
+		# SQL Tests
 		connection_timeout=0.1
 		read_timeout=5.0
 		#r = requests.post(url, data=sql_data, headers=headers, timeout=(connection_timeout,read_timeout) )
 		r = requests.get(url, headers=headers)
-		# Mongo prototype
+		
+		# Mongo tests
 		#print url, mongo_data
 		#r = requests.post(url, data=mongo_data, headers=headers)
-		
 
-		# Current system
-		#url = "https://azul@dev-00.azul.me:2048/search"
-		#r = requests.post(url, data=es_data, headers=headers )
-		
-		print r
-		#content = json.loads(r.content)
-		#print 'Prices: ' +content['prices'][0]
-		
-		
-		
+		content = json.loads(r.content)
+
 	except Exception,e: 
 		print 'Error: ' + str(e)
 		
-
-
 # Setup a list of processes that we want to run
-processes = [mp.Process(target=getAzul) for x in range(n)]
+processes = [mp.Process(target=GET) for x in range(n)]
 
 # Run processes
 for p in processes:
